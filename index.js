2ad492b31e1b7a148c3afadcd05bf0bc0fce4ed5
Sohail Ahmed <ahnrd43@gmail.com>
2022-11-05 13:43:02 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName,userEmail) {
+   return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(
+        {
+            name: userName,
+            email: userEmail
+        }
+    )
+   })
+   .then(resp => resp.json())
+   .then(obj => {
+    const id = obj.id
+    console.log(id)
+    const idNumber = document.querySelector(".userId");
+    idNumber.textContent = id;
+   })
+   .catch(error => {
+    const errorMessage = error.message
+    const errorP = document.querySelector(".errorP")
+    errorP.textContent = errorMessage
+   })
+}
+
